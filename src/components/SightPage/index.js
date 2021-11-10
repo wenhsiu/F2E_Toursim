b1d45578eb4102ba08865d6fcc768677b409713c
Wen-Hsiu Hsu <alvina8418@gmail.com>
2021-11-11 00:18:19 +0800

    add Carousel

--- src/components/SightPage/index.js
+++ src/components/SightPage/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Card, CardMedia, Container, Grid, Typography } from '@material-ui/core';
+import { Container, Grid, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import SightBackground from '../../assets/sight_background.png';
 import sunsetBackground from '../../assets/sunset_background.png';
-import Wave from '../../assets/wave.png';
-import NorthSalt from '../../assets/north_salt.png';
 import SmallCard from './SmallCard';
+import Carousel from 'react-material-ui-carousel';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -14,35 +13,98 @@ const useStyles = makeStyles((theme) =>
       backgroundSize: '100% 100%',
       height: 1020,
       maxHeight: 1020,
-      // '&:after': {
-      //   content: '""',
-      //   backgroundImage: `url(${Wave})`,
-      //   backgroundSize: '100% 680px',
-      //   position: 'absolute',
-      //   top: 48,
-      //   height: 680,
-      //   width: '100%',
-      // },
     },
     tabTitle: {
       fontSize: 24,
       fontWeight: theme.typography.fontWeightMedium,
-      margin: 72,
-      position: 'absolute',
+      marginBottom: 72,
+      marginLeft: 72,
+      top: 72,
+      position: 'relative',
+    },
+    carousel: {
+      // margin: 'auto',
+    },
+    carouselItemGroup: {
+      margin: 'auto',
+      justifyContent: 'center',
+    },
+    carouselItem: {
+      margin: 10,
+    },
+    activeIndicator: {
+      color: theme.colors.darkNavy,
+    },
+    navButton: {
+      backgroundColor: theme.colors.darkNavy,
     },
   })
 );
 
 const SightPage = () => {
   const classes = useStyles();
+
   return (
     <Grid className={classes.container}>
       <Container fixed disableGutters>
         <Grid item>
           <Typography className={classes.tabTitle}>水的景點</Typography>
         </Grid>
-        <Grid item>
-          <SmallCard />
+        <Grid item className={classes.carousel}>
+          <Carousel
+            autoPlay={false}
+            navButtonsAlwaysVisible={true}
+            animation="slide"
+            duration={1000}
+            // cycleNavigation={false}
+            // NextIcon={<NextIcon />}
+            // PrevIcon={<PrevIcon />}
+            navButtonsProps={{ className: classes.navButton }}
+            activeIndicatorIconButtonProps={{ className: classes.activeIndicator }}
+            // next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
+            // prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}
+          >
+            <Grid container className={classes.carouselItemGroup}>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+            </Grid>
+            <Grid container className={classes.carouselItemGroup}>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+              <Grid item className={classes.carouselItem}>
+                <SmallCard />
+              </Grid>
+            </Grid>
+          </Carousel>
         </Grid>
       </Container>
     </Grid>
