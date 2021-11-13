9cc98aa61c8c7dd2c30e8e5a75bc17b25626f331
Wen-Hsiu Hsu <alvina8418@gmail.com>
2021-11-13 22:14:47 +0800

    add place small card

--- src/components/SightPage/SmallCard.js
+++ src/components/SightPage/SmallCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardContent, CardMedia, Grid, Link, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/styles';
-import Ocean from '../../assets/ocean.png';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) =>
@@ -61,31 +60,22 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-const SmallCard = ({
-  image = Ocean,
-  name = '北門瀉湖',
-  detail = '雙春濱海遊憩區是位在台南北門的生態遊憩場所，園區內規劃紅樹林觀察區，網羅台灣珍貴的4種原生北門區之生態環境屬於潟湖生態，蘊含著豐富之魚、蝦、貝類及紅樹林景觀，常見許多鷺鳥、水鳥，彈塗魚和各種螃蟹，具有潟湖觀光生態旅遊之魅力。最讓人注目的是每年10月到翌年4月到此作客的黑腹燕鷗，每到黃昏成群的黑腹燕鷗會在空中群聚飛舞，依照風向變換各種不同隊型，相當壯觀。海茄苳紅樹林曾經大量生長在北門蚵寮西邊的北門潟湖區沼澤地帶，後來開發為海埔新生地或闢為魚塭，經常可見許多蚵架插置潟湖中，形成另一特殊的海岸景觀。在台南政府全力復育紅樹林的政策下，潟湖南邊之頭港大排水，出海口長有面積約6公頃的海茄苳；在急水溪出海口淤沙較多的北門潟湖區海茄苳也再度繁殖，並引進種植水筆仔，在這兩個據點設置紅樹林保護區，以翠綠的生態景觀驚豔遊客。',
-  // info = {
-  //   address: '臺南市709安南區城安路160號',
-  //   phone: '886-6-7861000',
-  //   openTime: '全年皆可，無時間限制(00:00~24:00)',
-  // },
-}) => {
+const SmallCard = ({ Picture, Name, Description }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.container}>
-      <CardMedia className={classes.image} image={image} title="" />
+      <CardMedia className={classes.image} image={Picture?.PictureUrl1} title="" />
       <CardContent>
         <Grid container direction="column">
           <Grid item className={classes.name}>
             <Typography align="center" className={classes.nameText}>
-              {name}
+              {Name}
             </Typography>
           </Grid>
           <Grid item className={classes.detail}>
             <Typography align="justify" className={classes.detailText}>
-              {detail}
+              {Description}
             </Typography>
           </Grid>
           <Grid item>
@@ -100,10 +90,9 @@ const SmallCard = ({
 };
 
 SmallCard.propTypes = {
-  image: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  detail: PropTypes.string,
-  info: PropTypes.object,
+  Picture: PropTypes.object,
+  Name: PropTypes.string,
+  Description: PropTypes.string,
 };
 
 export default SmallCard;
--- src/components/SightPage/index.js
+++ src/components/SightPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Container, Grid, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import SightBackground from '../../assets/sight_background.png';
@@ -6,6 +6,8 @@ import sunsetBackground from '../../assets/sunset_background.png';
 import SmallCard from './SmallCard';
 import Carousel from 'react-material-ui-carousel';
 import { TABS } from '../../constants/general';
+import { getSightInfo } from '../../service/toursium';
+import { placeListLocation } from '../../data/sightsLocation';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -44,6 +46,49 @@ const useStyles = makeStyles((theme) =>
 
 const SightPage = () => {
   const classes = useStyles();
+  const [places, setPlaces] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const list = await Promise.all(placeListLocation.map((location) => getSightInfo(location)));
+        setPlaces(list);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const formateCarouselItem = useMemo(() => {
+    if (!places) return [];
+    const pages = [];
+    let temp = [];
+
+    places.forEach((sight, i) => {
+      if (i !== 0 && i % 8 === 0) {
+        pages.push(
+          <Grid key={i} container className={classes.carouselItemGroup}>
+            {[...temp]}
+          </Grid>
+        );
+        temp = [];
+      }
+      temp.push(
+        <Grid key={sight.ID} item className={classes.carouselItem}>
+          <SmallCard {...sight} />
+        </Grid>
+      );
+    });
+
+    pages.push(
+      <Grid key={places.length} container className={classes.carouselItemGroup}>
+        {[...temp]}
+      </Grid>
+    );
+
+    return pages;
+  }, [places]);
 
   return (
     <Grid className={classes.container} id={TABS.PLACE}>
@@ -51,62 +96,19 @@ const SightPage = () => {
         <Grid item>
           <Typography className={classes.tabTitle}>水的景點</Typography>
         </Grid>
-        <Grid item className={classes.carousel}>
-          <Carousel
-            autoPlay={false}
-            navButtonsAlwaysVisible={true}
-            animation="slide"
-            duration={1000}
-            // cycleNavigation={false}
-            // NextIcon={<NextIcon />}
-            // PrevIcon={<PrevIcon />}
-            navButtonsProps={{ className: classes.navButton }}
-            activeIndicatorIconButtonProps={{ className: classes.activeIndicator }}
-            // next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
-            // prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}
-          >
-            <Grid container className={classes.carouselItemGroup}>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-            </Grid>
-            <Grid container className={classes.carouselItemGroup}>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-              <Grid item className={classes.carouselItem}>
-                <SmallCard />
-              </Grid>
-            </Grid>
-          </Carousel>
-        </Grid>
+        {places && (
+          <Grid item className={classes.carousel}>
+            <Carousel
+              autoPlay={false}
+              navButtonsAlwaysVisible={true}
+              animation="slide"
+              navButtonsProps={{ className: classes.navButton }}
+              activeIndicatorIconButtonProps={{ className: classes.activeIndicator }}
+            >
+              {formateCarouselItem}
+            </Carousel>
+          </Grid>
+        )}
       </Container>
     </Grid>
   );
