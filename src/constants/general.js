92b0148460f59243c394617b8e0d325050dfa956
Wen-Hsiu Hsu <alvina8418@gmail.com>
2021-11-14 19:57:51 +0800

    update path

--- src/constants/general.js
+++ src/constants/general.js
@@ -4,3 +4,10 @@ export const TABS = {
   MAP: 'map',
   PLACE: 'place',
 };
+
+export const PATH = {
+  MAIN: '/F2E_Toursim',
+  ABOUT: '/F2E_Toursim/about',
+  MAP: '/F2E_Toursim/map',
+  PLACE: '/F2E_Toursim/place',
+};
