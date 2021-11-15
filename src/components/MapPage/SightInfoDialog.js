b1b2e52bf622f7665b4b429084117cffe665deeb
Wen-Hsiu Hsu <alvina8418@gmail.com>
2021-11-15 23:47:54 +0800

    update dialog close icon

--- src/components/MapPage/SightInfoDialog.js
+++ src/components/MapPage/SightInfoDialog.js
@@ -1,36 +1,36 @@
-import { Dialog, Grid, IconButton } from '@material-ui/core';
+import { Dialog, IconButton } from '@material-ui/core';
 import React from 'react';
 import SightInfoCard from './SightInfoCard';
 import PropTypes from 'prop-types';
 import { ReactComponent as CloseIcon } from '../../assets/close_icon.svg';
 import { createStyles, makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    container: {
-      position: 'relative',
+    dialog: {
+      '& .MuiDialog-paperWidthSm': {
+        overflow: 'visible',
+      },
     },
     closeIcon: {
-      zIndex: 2000,
-      position: 'relative',
-      bottom: 186,
+      position: 'absolute',
+      justifyContent: 'flex-end',
+      padding: 0,
+      top: -16,
+      right: -16,
     },
   })
 );
 
 const SightInfoDialog = ({ open, onClose, ...props }) => {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Grid>
-      {/* {open && (
-        <IconButton aria-label="close" onClick={onClose} className={classes.closeIcon}>
-          <CloseIcon />
-        </IconButton>
-      )} */}
-      <Dialog open={open} onClose={onClose}>
-        <SightInfoCard {...props} />
-      </Dialog>
-    </Grid>
+    <Dialog open={open} onClose={onClose} className={classes.dialog}>
+      <IconButton aria-label="close" onClick={onClose} className={classes.closeIcon}>
+        <CloseIcon />
+      </IconButton>
+      <SightInfoCard {...props} />
+    </Dialog>
   );
 };
 
