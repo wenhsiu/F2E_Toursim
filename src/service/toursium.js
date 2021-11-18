98b9659aef5992e1ac8baccb4ed0fa421772d998
wenhsiu <38685420+wenhsiu@users.noreply.github.com>
2021-11-18 20:56:32 +0800

    update api key to .env (#11)

--- src/service/toursium.js
+++ src/service/toursium.js
@@ -2,18 +2,21 @@ import axios from 'axios';
 import jsSHA from 'jssha';
 
 const GetAuthorizationHeader = () => {
-  const AppID = '3b3a0c46f62548598dbc0d505f03cc0a';
-  const AppKey = 'yylcrlrrmCFayh4ugsPC8yG7aSk';
-
   const GMTString = new Date().toGMTString();
-  const ShaObj = new jsSHA('SHA-1', 'TEXT');
-  ShaObj.setHMACKey(AppKey, 'TEXT');
+  const ShaObj = new jsSHA('SHA-224', 'TEXT');
+  // eslint-disable-next-line no-undef
+  ShaObj.setHMACKey(process.env.REACT_APP_API_KEY, 'TEXT');
   ShaObj.update('x-date: ' + GMTString);
   const HMAC = ShaObj.getHMAC('B64');
   const Authorization =
-    'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+    'hmac username="' +
+    // eslint-disable-next-line no-undef
+    process.env.REACT_APP_API_ID +
+    '", algorithm="hmac-sha1", headers="x-date", signature="' +
+    HMAC +
+    '"';
 
-  return { Authorization: Authorization, 'X-Date': GMTString, 'Accept-Encoding': 'gzip' };
+  return { Authorization: Authorization, 'X-Date': GMTString };
 };
 
 export const getSightInfo = ({ latitude, longitude }) =>
